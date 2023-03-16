060172fae9cc5e8c564a1b67b7837bcab5a06fbd
Manan Patel <manan.patel2471@gmail.com>
2023-03-16 12:04:25 +0530

    work on video call project

--- app.js
+++ app.js
@@ -26,16 +26,60 @@ const mongoUrl =
   require("./userDetails");
     
 const User = mongoose.model("UserInfo");  
-var router = express.Router()
 
-app.use("/v1/",router);
-require("./routes/config")(router);
+app.post("/register", async (req, res) => {
+  const { fname, lname, email, password } = req.body;
+
+  const encryptedPassword = await bcrypt.hash(password, 10);
+  try {
+    const oldUser = await User.findOne({ email });            
+    if (oldUser) {
+     return res.json({ error: "User Exists" });
+    }
+     await User.create({
+      fname,
+      lname,
+      email,
+      password: encryptedPassword,
+    });
+    res.send({ status: "ok" });
+  } catch (error) {
+    res.send({ status: "error" });
+  }
+});
+
+app.post("/login-user", async (req, res) => {
+      const { email, password } = req.body;
+  
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res.json({ error: "User Not found" });
+      }
+      if (await bcrypt.compare(password, user.password)) {
+        const token = jwt.sign({}, JWT_SECRET);
+  
+        if (res.status(201)) {
+          return res.json({ status: "ok", data: token });
+        } else {
+          return res.json({ error: "error" });
+        }
+      }
+      res.json({ status: "error", error: "Invalid Password" });
+    });
 
 
-var router2 = express.Router()
 
-app.use("/v2/",router2)
-require("./routes/config")(router2);
+
+// var router = express.Router()
+
+// app.use("/v1/",router);
+// require("./routes/config")(router);
+
+
+// var router2 = express.Router()
+
+// app.use("/v2/",router2)
+// require("./routes/config")(router2);
 
 
 
